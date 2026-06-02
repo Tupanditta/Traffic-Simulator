8e8561de5a2994b715a67c40d1d082342e83a38d
Ander Lifeng Sola López <tupanditta@gmail.com>
2026-06-02 16:41:23 +0200

    feat: se ha terminado de codificar el módulo initialState.py y sus funciones (aunque se han introducido los datos manuales, esto hay que cambiarlo)

--- engine/initialState/initialState.py
+++ engine/initialState/initialState.py
@@ -5,19 +5,29 @@ Establecer un clima inicial por defecto para el día 0, de ahí calculará el mo
 climático el temporal del día uno
 
 Inicializar la estructura vacía donde iremos recopilando la información
+
+Nombrar las variables iniciales que necesito. Es mejor hacerlo en una función aparte, así el cuerpo base
+no tiene que entrar en el diccionario
 """
+import datetime
+
+def variables(initialState_Dict):
+  yesterday_date_dict = initialState_Dict
+  yesterday_date = yesterday_date_dict["Date"]["Date"]
+  final_date = initialState_Dict["Final Date"]
+
+  return yesterday_date, yesterday_date_dict, final_date
 
 def create_InitialState(context_dict):
   dictList = [] #los diccionarios de cada día se guardarán en un lista
   initialState_Dict = {
     "Date": {
-      "Year": 2017,
-      "Month": None,
-      "Day": None,
-      "Attribute": None,
-      "Season": None
+      "Date": datetime.date(2017, 12, 10),
+      "Attribute": datetime.date(2017, 12, 10).weekday(),
+      "Season": 2
     }, 
-    "Weather": None,
+    "Final Date": datetime.date(2018, 3, 19), #hay que cambiarlo, me lo he inventado
+    "Weather": "Rain",
     "Traffic": {
       "Total": None,
       "Children": None,
@@ -25,8 +35,5 @@ def create_InitialState(context_dict):
       "Adults": None,
       "Olders": None
     }, 
-
-  
-
   }
   return initialState_Dict, dictList
