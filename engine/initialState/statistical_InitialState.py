0cb196005a8e036ff9e9846b45e858c720500f04
Ander Lifeng Sola López <tupanditta@gmail.com>
2026-06-02 16:55:16 +0200

    ignore: por ahora he hecho solo el esquema, pero este módulo todavía no repercute en el flujo del simulador

---
+++ engine/initialState/statistical_InitialState.py
@@ -0,0 +1,23 @@
+"""
+Este módulo se encarga de determinar el inicio, día 0, de la simulación
+
+Es necesario para poder calcular ciertos datos del día 1 y sus siguientes 
+
+En este se especificarán los atributos del día weather y weekDay
+
+Voy a tener en cuenta que al inicio la simulación solo funicona en el añ0 2017, en Navarra
+"""
+
+
+def initialWeather(context_dict): 
+  temporality = context_dict.get("temporality", None)
+  return #weather
+
+def initialWeekDay(context_dict):
+  return #weekDay
+
+def initialDay(context_dict):
+  return 
+
+def calculate_weather(temporality):
+  return
